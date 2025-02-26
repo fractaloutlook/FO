1e45e580e1f54550faaa6f3dd1fb6bc86a550178
Tim Scheiman <timscheiman@gmail.com>
2025-02-26 09:12:44 -0600

    3.7 time let's go! 4

--- src/components/TestSDK.jsx
+++ src/components/TestSDK.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import initializeSpacetimeDB from '../../utils/spacetimeDBClient.jsx';
+//import initializeSpacetimeDB from '../../utils/spacetimeDBClient.jsx';
 
 const TestSDK = () => {
   const [sdkStatus, setSdkStatus] = useState('Not initialized');
