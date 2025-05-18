6538c05de33ad58a11b5b54cbfbc9bfe2ad653d7
Tim Scheiman <timscheiman@gmail.com>
2025-05-18 17:54:11 -0500

    did I break everything new server time

--- repubserver.bat
+++ repubserver.bat
@@ -1,3 +1,8 @@
-call plink opc@64.181.202.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private1.ppk" < "D:\CODE_REPOS\claudbiz\server\src\lib.rs" "cat > ~/status-module/src/lib.rs"
+@echo off
+REM Copy the lib.rs file to the server
+call plink ubuntu@149.130.211.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private2.ppk" "cat > ~/status-module/src/lib.rs" 0<"D:\CODE_REPOS\claudbiz\server\src\lib.rs"
 
-call plink opc@64.181.202.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private1.ppk" cd ~/status-module;spacetimedb-cli publish --project-path . status-module
+REM Publish the module (corrected command)
+call plink ubuntu@149.130.211.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private2.ppk" "cd ~/status-module;spacetimedb-cli publish --project-path . status-module"
+
+
