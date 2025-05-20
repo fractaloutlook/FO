f1787ffee3855b5d0335140c85df72aff9f6e764
Tim Scheiman <timscheiman@gmail.com>
2025-05-20 01:30:11 -0500

    new repubserver file

--- repubserver.bat
+++ repubserver.bat
@@ -1,8 +1,6 @@
 @echo off
 REM Copy the lib.rs file to the server
-call plink ubuntu@149.130.211.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private2.ppk" "cat > ~/status-module/src/lib.rs" 0<"D:\CODE_REPOS\claudbiz\server\src\lib.rs"
+call plink ubuntu@64.181.202.3 -no-antispoof -i "C:\Users\USER\My Documents\oracle\newdualsys.ppk" "cat > ~/status-module/src/lib.rs" 0<"D:\CODE_REPOS\claudbiz\server\src\lib.rs"
 
-REM Publish the module (corrected command)
-call plink ubuntu@149.130.211.144 -no-antispoof -i "C:\Users\USER\My Documents\oracle\oracle_private2.ppk" "cd ~/status-module;spacetimedb-cli publish --project-path . status-module"
-
-
+REM Publish the module using bash to load environment properly
+call plink ubuntu@64.181.202.3 -no-antispoof -i "C:\Users\USER\My Documents\oracle\newdualsys.ppk" "bash -l -c 'cd ~/status-module && spacetime publish --project-path . status-module'"
